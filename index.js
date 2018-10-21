8ba9fdee2a0b3df40e4aa01a3d432d84438b2372
David Dorward <david@dorward.me.uk>
2018-10-21 11:54:57 +0100

    Basic reading of credentials

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const {username, password, site } = require('./config.json')
+import obsidianportal from './lib/obsidian-portal'
+
+obsidianportal(username, password)
+
 process.stdout.write('test')
