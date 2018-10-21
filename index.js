aeccc9ae14a093bd3f3a345fe4e57b19a58009fd
David Dorward <david@dorward.me.uk>
2018-10-21 17:24:35 +0100

    Resize images and generate chart

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import obsidianportal from './lib/obsidian-portal'
 import graph from './lib/graph'
 import write from 'fs-writefile-promise'
 import getImages from './lib/getImages'
+import child_process from 'child_process'
 
 async function init () {
 	const api = await obsidianportal(username, password)
@@ -10,8 +11,18 @@ async function init () {
 	const [characters, pages] = await Promise.all([api.getAllCharacters(), api.getAllWikiPages()])
 	const entities = characters.concat(pages)
 	await getImages(entities)
+	await run('./resize.sh')
 	const dot = graph(entities)
 	await write('data.dot', dot)
+	await run('dot -T png -O data.dot')
+
 }
+
+async function run(command) {
+	return new Promise((res) => {
+		child_process.exec(command, function(error, stdout){ res(stdout) })
+	})
+}
+
 
 init()
---
+++ resize.sh
@@ -0,0 +1,2 @@
+mogrify -resize "96x96^" -gravity center -crop 96x96+0+0 +repage images/*
+
