dcd407b5e259465d4fe792887572c59e2ef18f48
David Dorward <david@dorward.me.uk>
2018-10-21 16:39:59 +0100

    Make connections and write to dot file

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 const {username, password, site } = require('./config.json')
 import obsidianportal from './lib/obsidian-portal'
 import graph from './lib/graph'
+import write from 'fs-writefile-promise'
 
 async function init () {
 	const api = await obsidianportal(username, password)
 	api.setSite(site)
 	const [characters, pages] = await Promise.all([api.getAllCharacters(), api.getAllWikiPages()])
 	const entities = characters.concat(pages)
-	log(entities)
 	const dot = graph(entities)
-	process.stdout.write(dot + '\n')
+	await write('data.dot', dot)
 }
 
 init()
-
-
-function log(x) {
-	console.log(JSON.stringify(x, (key, value) => {
-		if (key === '_api') {
-			return undefined
-		} else {
-			return value
-		}
-	}, 2))
-
-}
