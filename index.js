2203690e40a73e42c712ebfd488905b4d11561b7
David Dorward <david@dorward.me.uk>
2018-10-21 12:11:22 +0100

    Basic login

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const {username, password, site } = require('./config.json')
 import obsidianportal from './lib/obsidian-portal'
 
-obsidianportal(username, password)
+async function init () {
+	const api = await obsidianportal(username, password)
+	api.setSite(site)
+	const characters = api.getAllCharacters()
+}
+
+init()
+
+
 
 process.stdout.write('test')
