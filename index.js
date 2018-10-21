0c4945a89978f0923c8635e4ca2567f21aab4429
David Dorward <david@dorward.me.uk>
2018-10-21 17:19:46 +0100

    Download images

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const {username, password, site } = require('./config.json')
 import obsidianportal from './lib/obsidian-portal'
 import graph from './lib/graph'
 import write from 'fs-writefile-promise'
+import getImages from './lib/getImages'
 
 async function init () {
 	const api = await obsidianportal(username, password)
 	api.setSite(site)
 	const [characters, pages] = await Promise.all([api.getAllCharacters(), api.getAllWikiPages()])
 	const entities = characters.concat(pages)
+	await getImages(entities)
 	const dot = graph(entities)
 	await write('data.dot', dot)
 }
