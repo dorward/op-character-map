e4e8cac02205e39c5486e7f7428218d8634b5efd
David Dorward <david@dorward.me.uk>
2018-10-21 16:14:48 +0100

    Start generating a graph

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const {username, password, site } = require('./config.json')
 import obsidianportal from './lib/obsidian-portal'
+import graph from './lib/graph'
 
 async function init () {
 	const api = await obsidianportal(username, password)
 	api.setSite(site)
 	const [characters, pages] = await Promise.all([api.getAllCharacters(), api.getAllWikiPages()])
 	const entities = characters.concat(pages)
-	// characters.forEach(character => {
-	// 	console.log(character.name, character.connections.map(node => node.link))
-	// })
 	log(entities)
+	const dot = graph(entities)
+	process.stdout.write(dot + '\n')
 }
 
 init()
