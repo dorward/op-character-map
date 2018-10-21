2e023219b043df6d42cbcb92f673f384a0aed0ab
David Dorward <david@dorward.me.uk>
2018-10-21 15:46:57 +0100

    Fetch data from all wikipages

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ import obsidianportal from './lib/obsidian-portal'
 async function init () {
 	const api = await obsidianportal(username, password)
 	api.setSite(site)
-	const characters = await api.getAllCharacters()
-	characters.forEach(character => {
-		console.log(character.name, character.linkedNodes)
-	})
+	// const [characters, pages] = await Promise.all(api.getAllCharacters(), api.getAllWikiPages())
+	// characters.forEach(character => {
+	// 	console.log(character.name, character.connections.map(node => node.link))
+	// })
+    
+	const pages = await api.getAllWikiPages()
+	log(pages)
 	// console.log(characters)
 }
 
 init()
+
+
+function log(x) {
+	console.log(JSON.stringify(x, (key, value) => {
+		if (key === '_api') {
+			return undefined
+		} else {
+			return value
+		}
+	}, 2))
+
+}
