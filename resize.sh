a71f1c706f85e05123c360ff501f6ae7641992ef
David Dorward <david@dorward.me.uk>
2019-07-19 21:58:00 +0100

    More reliably resize script

--- resize.sh
+++ resize.sh
@@ -1,2 +1,3 @@
-mogrify -resize "96x96^" -gravity center -crop 96x96+0+0 +repage images/*
-
+for x in  images/*.jpeg images/*.jpg images/*.gif images/*.png;
+do mogrify -resize "96x96^" -gravity center -crop 96x96+0+0 +repage $x;
+done
